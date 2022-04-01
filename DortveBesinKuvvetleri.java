bc9d8b1552dc916fbb7af96a9f9a6fc55dc9fc2b
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-01 17:07:37 +0300

    Add files via upload

---
+++ DortveBesinKuvvetleri.java
@@ -0,0 +1,22 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class DortveBesinKuvvetleri {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sınır Sayısını Giriniz : ");
+        n = input.nextInt();
+
+        for ( int i = 1 ; i<= n;i*=4) {
+            System.out.print(i + ",");
+        }System.out.println(" (4'ün Katları)  ");
+
+
+        for ( int i = 1 ; i<= n;i*=5){
+            System.out.print(i + ",");
+        }System.out.println(" (5 in Katları)  ");
+    }
+}
