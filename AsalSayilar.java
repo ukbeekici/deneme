ade509cd9584d9d66e67c71d80f3236d48f905e3
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-05 10:59:55 +0300

    Add files via upload

---
+++ AsalSayilar.java
@@ -0,0 +1,15 @@
+package Donguler;
+
+public class AsalSayilar {
+    public static void main(String[] args) {
+        int asal = 0, sayac = 0, a = 0;
+        for (int i = 2; i < 100; i++) {
+            for (int j = 2; j < 100; j++)
+                if (i % j == 0)
+                    sayac++;
+            if (sayac < 2)
+                System.out.print(i + " ");
+            sayac = 0;
+        }
+    }
+}
