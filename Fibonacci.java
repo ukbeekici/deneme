f5b1548e688e46ebe003c7e9498dbd9011fe6870
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-05 11:09:03 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,32 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Eleman sayısını giriniz: ");
+        int elemansayisi=input.nextInt();
+        int sayi1=0;
+        int sayi2=1;
+        int toplam=1;
+        if (elemansayisi==1)
+            System.out.println("0");
+        else if (elemansayisi==2) {
+            System.out.println("0");
+            System.out.println("1");
+        }
+        else if (elemansayisi>1) {
+            System.out.println("0");
+            System.out.println("1");
+            int i = 1;
+            while (i <= elemansayisi-2) {
+                toplam = sayi1 + sayi2;
+                sayi1 = sayi2;
+                sayi2=toplam;
+                System.out.println(toplam);
+                i++;
+            }
+        }
+    }
+}
