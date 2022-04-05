52d5b29bc1138500b2d5d697d17e205b3094c18d
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-05 10:51:36 +0300

    Add files via upload

---
+++ MükemmelSayi.java
@@ -0,0 +1,21 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class MükemmelSayi {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int sayi=input.nextInt();
+        int toplam=0;
+        for (int i=1; i<sayi; i++){
+            if (sayi%i==0){
+                toplam+=i;
+            }
+        }
+        if (toplam==sayi)
+            System.out.println("Girdiğiniz sayı mükemmel sayıdır.");
+        else
+            System.out.println("Girdiğiniz sayı mükemmel sayı değildir.");
+    }
+}
