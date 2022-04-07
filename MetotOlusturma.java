b6ca9a1107093b32653cf55fb1997025fbe88601
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-07 09:50:02 +0300

    Add files via upload

---
+++ MetotOlusturma.java
@@ -0,0 +1,29 @@
+package Metodlar;
+
+import java.util.Scanner;
+
+public class MetotOlusturma {
+    static void PatternCreator (int number)
+    {
+        if (number<=0) {
+            System.out.print(" "+number);
+            return;
+        }
+        System.out.print(" "+number);
+        PatternCreator(number-5);
+        System.out.print(" "+number);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a;
+        while (true) {
+            System.out.print("N sayısı : ");
+            a=inp.nextInt();
+            System.out.print("Çıktısı : ");
+            PatternCreator(a);
+            System.out.println("\n");
+        }
+    }
+}
