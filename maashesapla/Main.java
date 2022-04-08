b1090f133cda0ccd215573e084df06dbc548d306
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-08 14:00:12 +0300

    Create Main.java

---
+++ maashesapla/Main.java
@@ -0,0 +1,14 @@
+package MaasHesaplama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        Employee em= new Employee("Haydar",10000,45,2013);
+        em.toStringg();
+
+
+
+    }
+}
