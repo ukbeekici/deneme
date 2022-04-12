e94ed10070a393d437e2d0a84c16be5a5b48669f
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-12 14:19:59 +0300

    Add files via upload

---
+++ HarmonikOrtalama.java
@@ -0,0 +1,15 @@
+package Diziler;
+
+import java.util.Arrays;
+
+public class HarmonikOrtalama {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double total = 0.0;
+        for (int i : numbers){
+            total+=1/i;
+        }
+        System.out.println("Dizi : \n"+ Arrays.toString(numbers ));
+        System.out.println("Dizinin Harmonik Ortalaması : " + numbers.length/total  );
+    }
+}
