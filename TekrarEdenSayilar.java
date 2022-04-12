69782513a6f8d75a471209a9f7a7ff1250f99619
ukbeekici <ukbe_ekici@hotmail.com>
2022-04-12 14:36:25 +0300

    Add files via upload

---
+++ TekrarEdenSayilar.java
@@ -0,0 +1,27 @@
+package Diziler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TekrarEdenSayilar {
+    public static void main(String[] args) {
+        int[] arrayToCheck = {12, -2, 3, 12, -2, 1, 2, 3, 5, 2, 7, 9, 11, 0, 2, 6, 5, 22, 4, 6, 7, 8, 8, 12};
+
+        ArrayList<Integer> newArray = new ArrayList<>();
+        ArrayList<Integer> duplicateItems = new ArrayList<>();
+
+        for (int i: arrayToCheck) {
+            if (!newArray.contains(i)) {
+                newArray.add(i);
+            } else {
+                if (i % 2 == 0 && !duplicateItems.contains(i)) {
+                    duplicateItems.add(i);
+                }
+            }
+        }
+
+        System.out.println("Array to check: " + Arrays.toString(arrayToCheck));
+        System.out.println("Duplicate even items are: " + duplicateItems);
+
+    }
+}
